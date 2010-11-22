aa8ca2459662d40d3ec6002567f22d241a8034c8
Michel Kraemer <michel@spamihilator.com>
2010-11-22 21:20:28 +0100

    Download ganymed library automatically

--- project/build/UcsyncProject.scala
+++ project/build/UcsyncProject.scala
@@ -2,5 +2,14 @@ import sbt._
 import java.net.URL
 
 class UcsyncProject(info: ProjectInfo) extends DefaultProject(info) {
-  
+  //download ganymed if needed
+  val ganymedVersion = "build250"
+  val ganymedJarPath = "ganymed-ssh2-" + ganymedVersion
+  val ganymedJar = "ganymed-ssh2-" + ganymedVersion + ".jar"
+  val ganymedPath = dependencyPath / ganymedJarPath / ganymedJar
+  val ganymedURL = new URL("http://www.cleondris.ch/ssh2/ganymed-ssh2-" + ganymedVersion + ".zip")
+  if (!ganymedPath.exists) {
+    log.info("Downloading and extracting Ganymed SSH-2 for Java " + ganymedVersion + " ...")
+    FileUtilities.unzip(ganymedURL, dependencyPath, ganymedJarPath + "/" + ganymedJar, log)
+  }
 }
